d97ff748cdb67c4628c025e47655bc1ad37b0eef
Andrey Volkov <andrew.wolf.alex@gmail.com>
2024-01-23 22:11:46 +0100

    Remove ARM bindings

--- onnxruntime-sys/src/generated/bindings.rs
+++ onnxruntime-sys/src/generated/bindings.rs
@@ -9,9 +9,3 @@ include!(concat!(
     env!("CARGO_MANIFEST_DIR"),
     "/src/generated/linux/aarch64/bindings.rs"
 ));
-
-#[cfg(all(target_os = "linux", target_arch = "arm"))]
-include!(concat!(
-    env!("CARGO_MANIFEST_DIR"),
-    "/src/generated/linux/arm/bindings.rs"
-));
